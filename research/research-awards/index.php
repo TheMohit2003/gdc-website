ed92ee4b93dfe0406b2db8b8fb545c958a5bbf24
khanzyd <khanzyd7@gmail.com>
2024-07-10 21:06:11 +0530

    research tab

--- research/research-awards/index.php
+++ research/research-awards/index.php
@@ -2,7 +2,7 @@
 
 $ROOT = "../../"; 
 include($ROOT . "includes/_init.php");
-$CURRENTDIRURL = $ROOTURL . "";
+$CURRENTDIRURL = $ROOTURL . "research/research-awards/";
 
 ?>
 
@@ -18,12 +18,37 @@ $CURRENTDIRURL = $ROOTURL . "";
     <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/global.css"></link>
     <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/_navbar.css"></link>
     <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/_footer.css"></link>
+    <link rel="stylesheet" href="<?php echo $ROOTURL ?>public/css/departments/departmentstyles.css"></link>
 </head>
 <body>
 
     <?php include($ROOT . "includes/_navbar.php"); ?>
 
-    <?php include($ROOT . "includes/_maintenance.php"); ?>
+    <div class="pageBanner">
+        <img src="<?php echo $ROOTURL ?>public/assets/pageBanner.jpg"/>
+        <h1>
+            RESEARCH AWARDS
+        </h1>
+    </div>
+    <div class="contentContainer">
+        <div>
+            <h1>MUHS state level Online Conference</h1>
+            <p>
+                MUHS state level Online Conference was held by Terna dental college, Navi Mumbai on 4th march 2024 wherein 7 Participants (2 UGs, 2 PGs, 2 staff members and 1 PhD Scholar) were guided with selection of research topic & PowerPoint presentations and because of the able guidance, the college received prizes which are as follows:
+            </p>
+            <ul class="list_inside">
+                <li>Staff category - 2nd Prize</li>
+                <li>UG student category - 1st Prize</li>
+                <li>PhD Scholar category - 1st Prize</li>
+            </ul>
+        </div>
+
+        <div>
+            <h2>MUHS ‘AVISHKAR’ 2023</h2>
+            <p>
+                MUHS conducted MUHS ‘AVISHKAR’ 2023 on 5th & 6th of December 2023 at MUHS Nashik in which 13 students (UG & PG) participated & were mentored for selection of Research Topics & PowerPoint presentations. They did excellent presentations in the conference.</p>
+        </div>
+    </div>
 
     <?php include($ROOT . "includes/_footer.php"); ?>
 
